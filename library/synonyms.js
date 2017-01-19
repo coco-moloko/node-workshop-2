1efb1a35c0cc8090e5927490c8228fb03cf0ab85
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-19 01:00:19 +0000

    added function to library/synonyms.js

--- library/synonyms.js
+++ library/synonyms.js
@@ -1,1 +1,28 @@
 //my api key = http://words.bighugelabs.com/api/2/3b86a6bba91c9e5365404342250efb80/word/json
+var request = require('request');
+
+function SynonymAPI(apiKey) {
+    this.apiKey = apiKey;
+}
+
+SynonymAPI.prototype.getSynonyms = function(word, callback) {
+    var wordURL = "http://words.bighugelabs.com/api/2/297c3daf8068381fabaddff1384d0867/" + word + "/json";
+    
+    request(wordURL, function(error, result) {
+        if (error) {
+            callback(error);
+        }
+        else {
+            try {
+                var synonyms = JSON.parse(result.body);
+                callback(null, synonyms);
+            }
+            catch (error) {
+                callback(error);
+            }
+        }
+    });
+};
+
+//==========EXPORTING===========================
+module.exports = SynonymAPI;
