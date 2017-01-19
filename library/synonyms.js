409aaee45ad7549102eabff367769fe67cba1896
Coco Moloko <courtneyb.brecht@gmail.com>
2017-01-19 01:05:37 +0000

    slight changes to my synonyms.js file

--- library/synonyms.js
+++ library/synonyms.js
@@ -6,7 +6,7 @@ function SynonymAPI(apiKey) {
 }
 
 SynonymAPI.prototype.getSynonyms = function(word, callback) {
-    var wordURL = "http://words.bighugelabs.com/api/2/297c3daf8068381fabaddff1384d0867/" + word + "/json";
+    var wordURL = "http://words.bighugelabs.com/api/2/3b86a6bba91c9e5365404342250efb80/" + word + "/json";
     
     request(wordURL, function(error, result) {
         if (error) {
